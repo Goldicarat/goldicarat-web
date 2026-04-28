e957bffaf0adfde6add139abd05d87b3c8119f32
goldi carat <goldicarat@gmail.com>
2026-04-28 18:48:06 +0530

    Update code folder

---
+++ admin/public/data.js
@@ -0,0 +1,15 @@
+// ******* Goldicarat *********
+
+// # Hostinger Login
+// Email: rbstarjewels@gmail.com
+// Password: Aaru@99044
+
+
+// # Github Login
+// Email: goldicarat@gmail.com
+// Password: Goldicarat@759#957
+
+
+// # Render Login
+// Email: goldicarat@gmail.com
+// Password: Goldicarat@759#957
